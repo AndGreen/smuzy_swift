b76f8d89cbd38ed86dc1fa44967a852046da6bc6
Andrew <zelenin.andrew@gmail.com>
2023-08-12 13:43:10 +0400

    Initial Commit

---
+++ smuzy/smuzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  smuzyApp.swift
+//  smuzy
+//
+//  Created by Andrey Zelenin on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct smuzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
